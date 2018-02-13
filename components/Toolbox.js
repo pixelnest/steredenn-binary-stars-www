78b46a0adaa18cc244b45127374994c13b3369ca
Matthieu Oger <moger@pixelnest.io>
2018-02-13 20:05:09 +0100

    Add components toolbox

---
+++ components/Toolbox.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+// -------------------------------------------------------------
+// Components.
+// -------------------------------------------------------------
+
+export const Aligner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+`
