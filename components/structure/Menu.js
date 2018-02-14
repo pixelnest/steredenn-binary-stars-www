50729caf24fc87b6b98d55950e0de32ec9de830e
Matthieu Oger <moger@pixelnest.io>
2018-02-14 19:00:46 +0100

    Improve menu with animations

--- components/structure/Menu.js
+++ components/structure/Menu.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 import {rgba} from 'polished'
 
 import Link from '../Link'
@@ -18,30 +18,55 @@ const MenuTitle = styled.h2`
 
 const MenuItem = styled.li`
   margin-bottom: 1rem;
-  padding: 1.3rem 2rem 1rem;
   font-size: 1.125em;
   text-transform: uppercase;
   line-height: normal;
-`
 
-const SocialMenuItem = MenuItem.extend`
-  background: linear-gradient(to right, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0));
-`
+  a {
+    display: block;
+    padding: 1.3rem 2rem 1rem;
 
-const StoreMenuItem = MenuItem.extend`
-  background: ${props =>
-    createColoredGradient(props.color ? props.color : 'black')};
+    background: ${props => createColoredGradient(props.color, props.direction)};
+    transition: all 0.25s ease-out;
+  }
+
+  a:hover {
+    ${props => getHoverPadding(props.direction)};
+
+    background: ${props =>
+      createColoredGradient(
+        props.hoverColor ? props.hoverColor : props.color,
+        props.direction,
+        0.5
+      )};
+  }
 `
 
 // -------------------------------------------------------------
 // Functions.
 // -------------------------------------------------------------
 
-function createColoredGradient(color) {
-  const first = rgba(color, 0)
-  const last = rgba(color, 0.25)
+function getHoverPadding(direction) {
+  if (direction === 'right') {
+    return css`
+      padding-right: 3rem;
+    `
+  } else {
+    return css`
+      padding-left: 3rem;
+    `
+  }
+}
 
-  return `linear-gradient(to right, ${first}, ${last});`
+function createColoredGradient(
+  color = 'black',
+  direction = 'left',
+  force = 0.25
+) {
+  const first = rgba(color, 0)
+  const last = rgba(color, force)
+
+  return `linear-gradient(to ${direction}, ${first}, ${last});`
 }
 
 // -------------------------------------------------------------
@@ -53,20 +78,20 @@ export const SocialMenu = ({}) => {
     <Menu>
       <MenuTitle>Social</MenuTitle>
       <ul>
-        <SocialMenuItem>
+        <MenuItem>
           <Link url="mailto:steredenn@pixelnest.io">Contact</Link>
-        </SocialMenuItem>
-        <SocialMenuItem>
+        </MenuItem>
+        <MenuItem>
           <Link url="https://twitter.com/pixelnest/">Twitter</Link>
-        </SocialMenuItem>
-        <SocialMenuItem>
+        </MenuItem>
+        <MenuItem>
           <Link url="https://facebook.com/pixelneststudio/">Facebook</Link>
-        </SocialMenuItem>
-        <SocialMenuItem>
+        </MenuItem>
+        <MenuItem>
           <Link url="http://pixelnest.io/presskit/steredenn-binary-stars/">
             Presskit
           </Link>
-        </SocialMenuItem>
+        </MenuItem>
       </ul>
     </Menu>
   )
@@ -77,27 +102,27 @@ export const StoreMenu = ({}) => {
     <Menu align="right">
       <MenuTitle>Stores</MenuTitle>
       <ul>
-        <StoreMenuItem color="#C60518">
+        <MenuItem color="#C60518" direction="right">
           <Link url="#">Nintendo Switch</Link>
-        </StoreMenuItem>
-        <StoreMenuItem color="#F1BD28">
+        </MenuItem>
+        <MenuItem color="#F1BD28" direction="right">
           <Link url="http://store.steampowered.com/app/347160">Steam</Link>
-        </StoreMenuItem>
-        <StoreMenuItem color="#49B4FF">
+        </MenuItem>
+        <MenuItem color="#49B4FF" direction="right">
           <Link url="https://store.playstation.com/#!/steredenn/cid=EP1302-CUSA04998_00-STEREDENN0000PXN">
             PS4
           </Link>
-        </StoreMenuItem>
-        <StoreMenuItem color="#98C73D">
+        </MenuItem>
+        <MenuItem color="#98C73D" direction="right">
           <Link url="http://store.xbox.com/en-US/Xbox-One/Games/Steredenn/d31b7d58-a407-4c4a-84ec-3dab1911a0c7">
             Xbox One
           </Link>
-        </StoreMenuItem>
-        <StoreMenuItem color="#B06FCB">
+        </MenuItem>
+        <MenuItem color="#B06FCB" direction="right">
           <Link url="https://itunes.apple.com/app/steredenn/id916957220">
             iPhone/iPad
           </Link>
-        </StoreMenuItem>
+        </MenuItem>
       </ul>
     </Menu>
   )
