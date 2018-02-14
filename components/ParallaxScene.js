54fc792ed77c958e62dc7aa1f86a483e930d97fe
Matthieu Oger <moger@pixelnest.io>
2018-02-14 14:49:39 +0100

    Rework parallax

--- components/ParallaxScene.js
+++ components/ParallaxScene.js
@@ -5,65 +5,94 @@ import styled from 'styled-components'
 import {Aligner} from './Toolbox'
 
 // -------------------------------------------------------------
+// Parallax layers.
+// -------------------------------------------------------------
+
+const Layer = styled.div`
+  position: absolute;
+  text-indent: -9999px;
+`
+
+const BackgroundLayer = Layer.extend`
+  top: 0;
+
+  height: 600px;
+
+  background: url('/static/images/parallax/background.png') repeat-x top;
+`
+
+const BigSunLayer = Layer.extend`
+  top: -1rem !important;
+
+  width: 100%;
+  height: 600px;
+
+  background: url('/static/images/parallax/big.png') no-repeat center;
+`
+
+const SmallSunShadowLayer = Layer.extend`
+  top: 130px !important;
+  left: 155px !important;
+
+  width: 279px;
+  height: 279px;
+
+  background: url('/static/images/parallax/small-shadow.png') no-repeat center;
+`
+
+const SmallSunLayer = Layer.extend`
+  top: 150px !important;
+  left: 200px !important;
+
+  width: 212px;
+  height: 238px;
+
+  background: url('/static/images/parallax/small.png') no-repeat center;
+`
+
+const SmallSunLightLayer = Layer.extend`
+  top: 180px !important;
+  left: 200px !important;
+
+  width: 193px;
+  height: 193px;
+
+  background: url('/static/images/parallax/small-light.png') no-repeat center;
+`
+
+const JunkLayer = Layer.extend`
+  top: 150px !important;
+  right: 150px;
+
+  height: 160px;
+
+  background: url('/static/images/parallax/junk.png') no-repeat right;
+`
+
+const LogoShadowLayer = Layer.extend`
+  left: 0;
+  right: 0;
+
+  height: 500px;
+
+  background: url('/static/images/parallax/logo-shadow.png') no-repeat center;
+
+  opacity: 0.75;
+`
+
+const LogoLayer = Layer.extend`
+  left: 0;
+  right: 0;
+
+  height: 500px;
+`
+
+// -------------------------------------------------------------
 // Components.
 // -------------------------------------------------------------
 
 const Scene = styled.div`
   height: 450px;
-
-  .layer-big-sun,
-  .layer-big-halo,
-  .layer-small-sun,
-  .layer-small-halo,
-  .layer-logo-shadow {
-    position: absolute;
-    text-indent: -9999px;
-  }
-
-  .layer-big-halo {
-    top: 0;
-    height: 600px;
-    background: url('/static/images/parallax/background.png') repeat-x top;
-  }
-
-  .layer-big-sun {
-    top: -1rem !important;
-    width: 100%;
-    height: 600px;
-    background: url('/static/images/parallax/big.png') no-repeat center;
-  }
-
-  .layer-small-halo {
-    top: 130px !important;
-    left: 155px !important;
-    width: 279px;
-    height: 279px;
-    background: url('/static/images/parallax/small-shadow.png') no-repeat center;
-  }
-
-  .layer-small-sun {
-    top: 150px !important;
-    left: 200px !important;
-    width: 212px;
-    height: 238px;
-    background: url('/static/images/parallax/small.png') no-repeat center;
-  }
-
-  .layer-logo-shadow,
-  .layer-logo-image {
-    height: 500px;
-  }
-
-  .layer-logo-shadow {
-    left: 0;
-    right: 0;
-    background: url('/static/images/parallax/logo-shadow.png') no-repeat center;
-  }
-
-  .layer-logo-image {
-    left: 0;
-    right: 0;
-  }
 `
 
 const Logo = styled.img`
@@ -78,11 +107,7 @@ const Logo = styled.img`
 
 export default class ParallaxScene extends Component {
   componentDidMount() {
-    this.parallax = new Parallax(this.scene, {
-      frictionX: 0.025,
-      frictionY: 0.01,
-      invertX: false
-    })
+    this.parallax = new Parallax(this.scene)
   }
 
   componentWillUnmount() {
@@ -92,20 +117,18 @@ export default class ParallaxScene extends Component {
   render() {
     return (
       <Scene innerRef={el => (this.scene = el)}>
-        <div className="layer-big-halo">Halo</div>
-        <div className="layer-big-sun" data-depth="0.05">
-          Sun
-        </div>
-        <div className="layer-small-halo" data-depth="0.25">
-          Small Halo
-        </div>
-        <div className="layer-small-sun" data-depth="0.25">
-          Small Sun
-        </div>
-        <div className="layer-logo-shadow" data-depth="0.95">
-          Logo Shadow
-        </div>
-        <div className="layer-logo-image" data-depth="1.00">
+        <BackgroundLayer data-depth="0.00" />
+
+        <BigSunLayer data-depth="0.05" />
+
+        <SmallSunShadowLayer data-depth="0.25" />
+        <SmallSunLayer data-depth="0.25" />
+        <SmallSunLightLayer data-depth="0.1" />
+
+        <JunkLayer data-depth="0.075" />
+
+        <LogoShadowLayer data-depth="0.5" />
+        <LogoLayer data-depth="0.01">
           <Aligner>
             <Logo
               src="/static/images/Logo.png"
@@ -113,7 +136,7 @@ export default class ParallaxScene extends Component {
               alt="Steredenn: Binary Stars"
             />
           </Aligner>
-        </div>
+        </LogoLayer>
       </Scene>
     )
   }
