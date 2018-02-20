68c8a8fd05e618374fd9e957233853cea218156e
Matthieu Oger <moger@pixelnest.io>
2018-02-20 18:52:16 +0100

    Export link styles

--- components/Link.js
+++ components/Link.js
@@ -1,25 +1,29 @@
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 import {Colors} from '../styles/values'
 
 // -------------------------------------------------------------
 // Components.
 // -------------------------------------------------------------
 
+export const linkStyles = css`
+  color: ${Colors.Brand};
+  transition: color 0.1s ease-in;
+
+  text-decoration: none;
+
+  &:hover {
+    color: ${Colors.BrandLight};
+  }
+
+  &:active,
+  &:focus {
+    color: ${Colors.BrandDark};
+  }
+`
+
 export default ({url, children}) => {
   const Item = styled.a`
-    color: ${Colors.Brand};
-    transition: color 0.1s ease-in;
-
-    text-decoration: none;
-
-    &:hover {
-      color: ${Colors.BrandLight};
-    }
-
-    &:active,
-    &:focus {
-      color: ${Colors.BrandDark};
-    }
+    ${linkStyles};
   `
 
   return <Item href={url}>{children}</Item>
