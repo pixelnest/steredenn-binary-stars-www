61a030ab3f5b91814cb48bea0a19e130a9b7f845
Matthieu Oger <moger@pixelnest.io>
2018-02-21 17:51:56 +0100

    Add faq and error to exports

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   exportPathMap: function() {
     return {
-      '/': {page: '/index'}
+      '/': {page: '/index'},
+      '/404/': {page: '/_error'},
+      '/faq/': {page: '/faq'}
     }
   }
 }
