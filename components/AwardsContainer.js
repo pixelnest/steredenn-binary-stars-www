8cf80f496f1223f9c11df3a8e2f917be492db513
Matthieu Oger <moger@pixelnest.io>
2018-03-29 16:17:04 +0200

    Normalize ratings

--- components/AwardsContainer.js
+++ components/AwardsContainer.js
@@ -56,19 +56,19 @@ export default function AwardsContainer() {
           url="http://www.pocketgamer.co.uk/r/iPad/Steredenn/review.asp?c=73946&srch=steredenn"
         />
         <SmallAward
-          rating="5/5"
+          rating="10/10"
           site="TouchArcade"
           url="http://toucharcade.com/2017/05/22/steredenn-review-horns-out-for-this-roguelike-shoot-em-up/"
         />
         <SmallAward
-          rating="4.5/5"
-          site="Windows Central"
-          url="http://www.windowscentral.com/steredenn-retro-take-saving-galaxy-space-pirates/"
-        />
-        <SmallAward
-          rating="5/5"
+          rating="10/10"
           site="Extralife"
           url="http://www.extralife.fr/test-jv/5285/steredenn-ils-ont-des-shmups-mignons-vive-les-bretons.html"
+        />
+        <SmallAward
+          rating="9/10"
+          site="Windows Central"
+          url="http://www.windowscentral.com/steredenn-retro-take-saving-galaxy-space-pirates/"
         />
       </Line>
       <Line>
@@ -87,16 +87,15 @@ export default function AwardsContainer() {
           site="Steam"
           url="http://store.steampowered.com/app/347160/Steredenn/"
         />
-
-        <SmallAward
-          rating="15/20"
-          site="jeuxvideo.com"
-          url="http://www.jeuxvideo.com/test/445407/stederenn-un-shooter-rogue-like-explosif.htm"
-        />
         <SmallAward
           rating="8/10"
           site="CanardPC"
           url="https://boutique.pressenonstop.com/anciens-numeros/voir/canard-pc-326-443"
+        />
+        <SmallAward
+          rating="7.5/10"
+          site="jeuxvideo.com"
+          url="http://www.jeuxvideo.com/test/445407/stederenn-un-shooter-rogue-like-explosif.htm"
         />
       </Line>
     </Container>
