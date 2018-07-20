96d01eeefb3c2dd69043cdd55dca9f552a0bfe6a
Matthieu Oger <moger@pixelnest.io>
2018-07-20 18:19:43 +0200

    Add wallpapers

--- README.md
+++ README.md
@@ -28,3 +28,19 @@ Then:
 ```
 yarn run deploy
 ```
+
+## Create thumbs
+
+`thumbs-50` means thumbnails of quality 50.
+`thumbs-80` means… thumbnails of quality 80. Oh yeah.
+
+Use imagemagick:
+
+```
+for i in *.png; convert $i -resize 400x225 -quality 50 thumbs-50/$i.jpg; end
+for i in *.png; convert $i -resize 400x225 -quality 80 thumbs-80/$i.jpg; end
+```
+
+You might need to remove a useless extension suffix.
+
+TODO: script that.
--- content/images.js
+++ content/images.js
@@ -40,3 +40,30 @@ export const gifs = {
   placeholderFolder: '/static/images/gifs/placeholders',
   data: ['10', '01', '04', '06', '07', '23', '18', '19', '21', '03']
 }
+
+export const wallpapers = {
+  label: 'Wallpaper',
+  extension: 'png',
+  thumbnailExtension: 'jpg',
+  folder: '/static/images/wallpapers',
+  thumbnailFolder: '/static/images/wallpapers/thumbs-50',
+  data: [
+    'art',
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16'
+  ]
+}
