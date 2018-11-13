da5f51eb5119a96bf53aecbb77300865d7cd6393
Matthieu Oger <moger@pixelnest.io>
2018-11-13 10:42:26 +0100

    Update status

--- content/progress.js
+++ content/progress.js
@@ -1,12 +1,15 @@
+/* Done:'#ED9301'*/
+/* Almost:'#01A1ED'*/
+/* Testing:'#FF5757'*/
 export default [
   {name: 'Nintendo Switch', progress: '1', text: 'Released', color: '#ED9301'},
   {
     name: 'Steam',
     progress: '1',
     text: 'Released',
-    color: '#ED9301' /*'#01A1ED'*/
+    color: '#ED9301'
   },
   {name: 'Xbox One', progress: '1', text: 'Released', color: '#ED9301'},
   {name: 'PlayStation 4', progress: '1', text: 'Released', color: '#ED9301'},
-  {name: 'iPhone/iPad', progress: '0.8', text: 'Testing', color: '#FF5757'}
+  {name: 'iPhone/iPad', progress: '0.9', text: 'Review', color: '#01A1ED'}
 ]
