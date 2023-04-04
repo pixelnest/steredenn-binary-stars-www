ba2cf26244cf8c09e27aa7533a6820e81b29f345
Matthieu Oger <moger@pixelnest.io>
2023-04-04 08:05:01 +0200

    Change Amplitude key depending on env

--- components/analytics.js
+++ components/analytics.js
@@ -3,12 +3,18 @@ import amplitudePlugin from '@analytics/amplitude'
 
 import Router from 'next/router'
 
+console.log(process.env.NODE_ENV)
+const apiKey =
+  process.env.NODE_ENV === 'production'
+    ? 'd0312186d2f2a1ab437d022311a046d5'
+    : 'b71a729a175ee4d976274d115fa4dc26'
+
 // Options: https://bit.ly/3dRdZnE
 const analytics = Analytics({
   app: 'steredenn-www',
   plugins: [
     amplitudePlugin({
-      apiKey: 'd0312186d2f2a1ab437d022311a046d5',
+      apiKey,
       options: {
         trackingOptions: {
           ip_address: false
